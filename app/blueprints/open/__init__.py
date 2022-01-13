9a299b9d533d7f8f952e834e843268f777d01ef2
JohannaUlveklint <johanna.ulveklint@gmail.com>
2022-01-13 12:47:41 +0100

    Added functions in user-blueprint for searching routes

--- app/blueprints/open/__init__.py
+++ app/blueprints/open/__init__.py
@@ -1,5 +1,9 @@
 import datetime
 
+from unidecode import unidecode
+from geopy.geocoders import Nominatim
+import requests
+
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import check_password_hash
@@ -80,5 +84,55 @@ def search_route():
 
 @bp_open.post('/searchroute')
 def search_route_post():
-    start = request.form.get('start')
-    end = request.form.get('end')
+    # start = request.form.get('start')
+    # end = request.form.get('end')
+    start = 'Runstensgatan 1 Göteborg'
+    end = 'Sommarvädersgatan 1 Göteborg'
+    s_lat, s_long, e_lat, e_long = lat_long(start, end)
+    route = get_route(s_lat, s_long, e_lat, e_long)
+    return route
+
+
+def get_route(s_lat, s_long, e_lat, e_long):
+    api_key = "5b3ce3597851110001cf6248d62eca3e4d314dba96c2e5596a0f8074"
+    if not all([s_long, s_lat, e_long, e_lat]):
+        print("Missing geo-coordinates")
+        return None
+
+    search = f'api_key={api_key}&start={s_long},{s_lat}&end={e_long},{e_lat}'
+    response = requests.get('https://api.openrouteservice.org/v2/directions/cycling-regular?' + search)
+    if not response.ok:
+        return 'Bad Request!'
+    response = response.json()
+    return response
+
+
+# def distance_duration(s_lat, s_long, e_lat, e_long):
+#     route = get_route(s_lat, s_long, e_lat, e_long)
+#     distance = route['features'][0]['properties']['segments'][0]['distance']
+#     duration = route['features'][0]['properties']['segments'][0]['duration']
+#     return distance, duration
+
+
+def lat_long(start, end):
+    geolocator = Nominatim(user_agent="my_application", timeout=15)
+    s = geolocator.geocode(start)
+    e = geolocator.geocode(end)
+    s_lat = str(s.latitude)
+    s_long = str(s.longitude)
+    e_lat = str(e.latitude)
+    e_long = str(e.longitude)
+    return s_lat, s_long, e_lat, e_long
+
+
+def remove_non_ascii(text):
+    return unidecode(text)
+
+
+def main():
+    route = search_route_post
+    print(route)
+
+
+if __name__ == '__main__':
+    main()
